8422c32eef17bc374c876587b74ab9b3d06ade40
panitakanaguduru <panitakanaguduru.18@gmail.com>
2025-03-02 18:46:56 -0500

    Updated base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "https://panitakanaguduru.github.io/Portfolio/", // Correct full GitHub Pages URL
 });
